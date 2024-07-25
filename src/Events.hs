b916b2ee7a836f28025ba3cb65c398c27ed0c705
asfarious <asfarious@gmail.com>
2024-07-25 18:07:54 +0300

    Fixed diagonal movement

--- src/Events.hs
+++ src/Events.hs
@@ -22,10 +22,15 @@ processEvents (MkMapState pos _ mMode)  input = (MkMapState pos' (Just cursor) m
           go GLFW.Key'M (v, _)     = (v, show pos ++ "\n")
           go _ diff   = diff
           dPos = keyboardMovement + V3 0 0 (negate . realToFrac $ (scrollInput input * mouseScrollSensitivity * zoomSpeed))
-          pos' = boundZoom (pos + dPos)
+          pos' = boundZoom (pos + normDPos dPos)
           cursor = case pos' of
             (V3 x z zoom) -> projectCursor (V3 x 0 z) zoom $ cursorPosition input
             
 boundZoom :: V3 Float -> V3 Float
 boundZoom (V3 x y s) = V3 x y s'
     where s' = max s zoomMinBound
+
+normDPos :: V3 Float -> V3 Float
+normDPos dPos@(V3 0 0 s) = dPos
+normDPos (V3 x y s) = V3 x' y' s
+    where (V2 x' y') = cameraSpeed *^ (signorm $ V2 x y)
