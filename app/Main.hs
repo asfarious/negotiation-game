f22ad2b0ba9a6a56bf181027967b865e28f4cb4b
asfarious <asfarious@gmail.com>
2024-07-28 20:56:45 +0300

    A primitive text output test

--- app/Main.hs
+++ app/Main.hs
@@ -16,6 +16,7 @@ import                         Projection                           (projectToBo
 import                         Input
 import                         States
 import                         Events
+import                         Text
 
 main :: IO ()
 main =
@@ -24,9 +25,20 @@ main =
         (GLFW.WindowConfig displayWidth displayHeight "The Negotiation Game" Nothing [GLFW.WindowHint'Resizable False] Nothing)
     
     mapTexture <- importTexture mapPath mapHeight mapWidth
-    (vertexBuffer, quadBuffer, rectBuffer, positionBuffer) <- initializeBuffers
+    (vertexBuffer, quadBuffer, rectBuffer, textQuadBuffer, charBuffer, positionBuffer) <- initializeBuffers
     let initialPosition = V3 (mapQuadWidth/2) (mapQuadHeight/2) initialZoom
     writeBuffer positionBuffer 0 [initialPosition]
+    
+    freetype <- liftIO $ textInit
+    defaultFont <- loadFont freetype defaultFontFile (V2 mapWidth mapHeight) 
+    maybeDefaultFont' <- loadCharacters defaultFont  ("#" ++ ['0'..'9'] ++ ['A'..'Z'] ++ ['А'..'Я'] ++ ['a'..'z'] ++ ['а'..'я'] ++ "!?.,() ")
+    let defaultFont' = case maybeDefaultFont' of
+                        Just font -> font
+                        Nothing   -> error "FONT ERROR!!!"
+        defaultChar = case getCharacter defaultFont' '#' of
+                        Right octothorp -> octothorp
+                        Left _          -> error "# NOT LOADED SUCCESSFULLY"
+    let atlas = atlasTexture defaultFont'
     
     isStrTVar'   :: TVar Bool       <- liftIO $ atomically $ newTVar (False :: Bool)
     strTVar'    :: TVar String      <- liftIO $ atomically $ newTVar ([] :: String)
@@ -38,19 +50,32 @@ main =
     GLFW.setScrollCallback win $ Just $ mouseScrollCallback scrollTVar'
     GLFW.setCharCallback win $ Just $ keyboardCallback strTVar' isStrTVar'
     
-    shader <- compileShader $ boardShader win positionBuffer mapTexture
+    shadeBoard <- compileShader $ boardShader win positionBuffer mapTexture
+    shadeText  <- compileShader $ textShader win atlas
     
     let mapState = MkMapState { position = initialPosition
                               , cursor   = Just (V4 0 0 0 1)
                               , mapMode  = RawMapMode
                               }
-      
-    gameLoop vertexBuffer positionBuffer quadBuffer rectBuffer shader win mapState inputTVars
+    
+    let testString = "Test string. Тестовая строка!"
+        testLength = length testString
+        testText = makeTextSprites defaultFont' (V3 0 0 0) (fromIntegral displayWidth) (fromIntegral displayHeight) defaultChar testString
+    writeBuffer charBuffer 0 testText
+    
+    
+    gameLoop vertexBuffer positionBuffer quadBuffer rectBuffer textQuadBuffer charBuffer 
+             shadeBoard shadeText win testLength
+             mapState 
+             inputTVars 
+             freetype defaultFont' defaultChar
 
 initializeBuffers :: (ContextHandler ctx, MonadIO m) => ContextT ctx os m 
                  (Buffer os (B4 Float, B2 Float), 
                   Buffer os (B4 Float), 
                   Buffer os (B3 Float, B4 Float), 
+                  Buffer os (B4 Float),
+                  Buffer os (B3 Float, B2 Float, B4 Float),
                   Buffer os (Uniform (B3 Float)))
 initializeBuffers = do
     vertexBuffer :: Buffer os (B4 Float, B2 Float) <- newBuffer 4
@@ -68,15 +93,29 @@ initializeBuffers = do
                              ] :: [V4 Float])
                              
     rectBuffer :: Buffer os (B3 Float, B4 Float) <- newBuffer 800 -- position + color
-    writeBuffer rectBuffer 0 $ repeat (V3 0 0 0 , V4 0 0 0 0)
+    writeBuffer rectBuffer 0 $ repeat (V3 0 0 0, V4 0 0 0 0)
+    
+    textQuadBuffer :: Buffer os (B4 Float) <- newBuffer 4
+    writeBuffer textQuadBuffer 0 [ V4 0 1 0 1
+                                 , V4 1 1 0 1
+                                 , V4 0 0 0 1
+                                 , V4 1 0 0 1
+                                 ]
+    
+    charBuffer :: Buffer os (B3 Float, B2 Float, B4 Float) <- newBuffer 800 -- 3D-position + sprite size + atlas offset + atlas chunk size
+    writeBuffer charBuffer 0 $ repeat (V3 0 0 0, V2 0 0, V4 0 0 0 0)
     
     positionBuffer :: Buffer os (Uniform (B3 Float)) <- newBuffer 1 -- Looking-at 2D-position + Zoom level
     writeBuffer positionBuffer 0 [0]
     
-    pure (vertexBuffer, quadBuffer, rectBuffer, positionBuffer) 
+    pure (vertexBuffer, quadBuffer, rectBuffer, textQuadBuffer, charBuffer, positionBuffer) 
 
 
-gameLoop vertexBuffer positionBuffer quadBuffer pointBuffer shader win mapState inputTVars = do
+gameLoop vertexBuffer positionBuffer quadBuffer pointBuffer textQuadBuffer charBuffer
+         shadeBoard shadeText win testLength
+         mapState 
+         inputTVars 
+         freetype defaultFont defaultChar = do
     -- Collect input --
     input <- collectInput win inputTVars
 
@@ -101,11 +140,20 @@ gameLoop vertexBuffer positionBuffer quadBuffer pointBuffer shader win mapState 
         quadArray <- newVertexArray quadBuffer
         let primitiveArray = toPrimitiveArray TriangleStrip vertexArray
         let pointPrimitives = toPrimitiveArrayInstanced TriangleStrip (\vertex (offset, color) -> (offset, vertex, color)) quadArray pointArray
-        shader (primitiveArray, pointPrimitives)
+        shadeBoard (primitiveArray, pointPrimitives)
+        
+        textQuadArray <- newVertexArray textQuadBuffer
+        charArray <- takeVertices testLength <$> newVertexArray charBuffer
+        let charPrimitives = toPrimitiveArrayInstanced TriangleStrip (\vertex (pos, size, atlasPos) -> (pos, vertex, size, atlasPos)) textQuadArray charArray
+        shadeText charPrimitives
   
     swapWindowBuffers win
     
     -- Do another iteration --
     closeRequested <- GLFW.windowShouldClose win
-    unless (closeRequested == Just True) $
-        gameLoop vertexBuffer positionBuffer quadBuffer pointBuffer shader win mapState' inputTVars
+    unless (closeRequested == Just True) $ 
+        gameLoop vertexBuffer positionBuffer quadBuffer pointBuffer textQuadBuffer charBuffer
+                 shadeBoard shadeText win testLength
+                 mapState' 
+                 inputTVars 
+                 freetype defaultFont defaultChar  
