68e46cd3e083bc544c1d854c6c67413dad1ec9c4
asfarious <asfarious@gmail.com>
2024-08-05 20:40:22 +0300

    Removed unneccessary lists from GUI DSL

--- src/GUI/DescribeGUI.hs
+++ src/GUI/DescribeGUI.hs
@@ -7,21 +7,17 @@ import                         Data.Foldable                    (foldMap')
 import                         GUI.GUIState
 import                         Events
 
-type DescribeGUI = Writer ([GUIEventHandler Event], [GUIRenderHandler]) --
+type DescribeGUI = Writer (GUIEventHandler Event, GUIRenderHandler)
 
 newPreElement :: DescribeGUI () -> BoundingBox -> PreGUIElement Event
 newPreElement description bBox = case execWriter description of
-                                    (eventHandlers, renderHandlers) -> ( bBox
-                                                                       , \x y -> foldMap' (\f -> f x y) eventHandlers
-                                                                       , \x y -> foldMap' (\f -> f x y) renderHandlers
-                                                                       )
-
+                                    (eventHandler, renderHandler) -> (bBox, eventHandler, renderHandler)
 
 guiBox :: (BoundingBox -> (Either (V2 Int) (V2 Float), Maybe (V3 Float)) -> GUIBox) -> DescribeGUI ()
-guiBox renderHandler = tell ([\_ _ -> []], [\x y -> [renderHandler x y]])
+guiBox renderHandler = tell (\_ _ -> [], \x y -> [renderHandler x y])
 
 onClick :: GUIEventHandler Event -> DescribeGUI ()
-onClick eventHandler = tell ([eventHandler], [\_ _ -> []])
+onClick eventHandler = tell (eventHandler, \_ _ -> [])
 
 coloredBackground :: V4 Float -> DescribeGUI ()
 coloredBackground color = guiBox $ \bBox _ -> ColoredBox bBox color
@@ -37,9 +33,8 @@ textBox :: V4 Int -> ((Either (V2 Int) (V2 Float), Maybe (V3 Float)) -> String) 
 textBox at text = guiBox $ \bBox@(V4 x y _ _) state -> TextBox (V4 x y 0 0 + at) $ text state
 
 coloredButton :: V4 Int -> V4 Float -> GUIEventHandler Event -> DescribeGUI ()
-coloredButton buttonBox@(V4 x y width height) color eventHandler = tell ( [\elemId click -> if isInBoundingBox (getCursor click) buttonBox
+coloredButton buttonBox@(V4 x y width height) color eventHandler = tell ( \elemId click -> if isInBoundingBox (getCursor click) buttonBox
                                                                             then eventHandler elemId click
                                                                             else []
-                                                                          ]
-                                                                        , [\(V4 boxX boxY _ _) _ -> [ColoredBox (V4 (boxX + x) (boxY + y) width height) color]]
+                                                                        , \(V4 boxX boxY _ _) _ -> [ColoredBox (V4 (boxX + x) (boxY + y) width height) color]
                                                                         ) 
