0601b2fe8f2c6364738eb6f199b373ddd3de85c1
asfarious <asfarious@gmail.com>
2024-08-05 20:49:54 +0300

    Removed redundant code

--- src/GUI/GUIElements.hs
+++ src/GUI/GUIElements.hs
@@ -26,34 +26,3 @@ clickWindowPreElement (V2 x y) = flip newPreElement (V4 0 200 555 50) $ do
                                                                 onClick $ \self click -> case click of
                                                                         GUILeftClick _  -> []
                                                                         GUIRightClick _ -> [Event'GUIEvent $ DeleteElement self]
-
-{---
-cursorStatusPreElement :: PreGUIElement Event
-cursorStatusPreElement = ( V4 0 0 555 90
-                         , \_ click -> case click of
-                                            GUIRightClick _-> []
-                                            GUILeftClick at -> [Event'GUIEvent $ CreateElement (clickWindowPreElement at)]
-                         , \bBox@(V4 bbX bbY _ _) (cur2D, cur3D) 
-                              -> let cur2DText = case cur2D of
-                                                      Left (V2 x y)  -> "2D Cursor: " ++ printf "%d, %d" x y
-                                                      Right (V2 x y) -> "2D Cursor: " ++ printf "%.2f, %.2f" x y
-                                     cur3DText = case cur3D of
-                                                      Just (V3 x y z) -> "3D Cursor: " ++ printf "%.2f, %.2f, %.2f" x y z
-                                                      Nothing         -> "Cursor is on GUI!"
-                                 in [ ColoredBox bBox (V4 1 0 0 1)
-                                    , TextBox (V4 (bbX+5) (bbY+40) 1 1) cur2DText
-                                    , TextBox (V4 (bbX+5) (bbY+80) 1 1) cur3DText
-                                    ]
-                         )
-
-clickWindowPreElement :: V2 Int -> PreGUIElement Event
-clickWindowPreElement (V2 x y) = ( V4 0 200 555 50
-                                 , \self click -> case click of
-                                                    GUILeftClick _  -> []
-                                                    GUIRightClick _ -> [Event'GUIEvent $ DeleteElement self]
-                                 , \bBox@(V4 bbX bbY _ _) _ ->
-                                    [ ColoredBox bBox (V4 1 1 0 1)
-                                    , TextBox (V4 (bbX+5) (bbY+40) 1 1) $ printf "Clicked at: %d, %d" x y
-                                    ]
-                                 )
---}
